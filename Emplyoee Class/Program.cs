15fecd7aea7f8225069426235e8f67404083cc50
Abdul Rahman <abdulrahmanmuddasser@gmail.com>
2023-04-24 22:40:39 +0500

    properties

--- Emplyoee Class/Program.cs
+++ Emplyoee Class/Program.cs
@@ -4,4 +4,23 @@
     private string firstName;
     private string lastName;
     private decimal monthlySalary;
+
+    // properties for each variables
+    public string FirstName
+    {
+        get { return firstName; }
+        set { firstName = value; }
+    }
+
+    public string LastName
+    {
+        get => lastName;
+        set => lastName = value;
+    }
+
+    public decimal MonthlySalary
+    {
+        get => monthlySalary;
+        set => monthlySalary = value;
+    }
 }
