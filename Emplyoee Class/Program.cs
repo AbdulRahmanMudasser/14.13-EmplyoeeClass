ef84642efbe61dedd67a4b70b61053471c76edfb
Abdul Rahman <abdulrahmanmuddasser@gmail.com>
2023-04-24 23:01:07 +0500

    yearly salary

--- Emplyoee Class/Program.cs
+++ Emplyoee Class/Program.cs
@@ -36,5 +36,12 @@
 
 class EmployeeTest
 {
+    static void Main(string[] args)
+    {
+        Employee employee1 = new Employee("Abdul", "Rahman", 7800.75m);
+        Employee employee2 = new Employee("Dua", "Fatima", 6300.30m);
 
+        Console.WriteLine("Employee One \n\nName: {0} {1} \nMonthly Salary: {2} \nYearly Salary: {3}", employee1.FirstName, employee1.LastName, employee1.MonthlySalary, employee1.MonthlySalary * 12);
+        Console.WriteLine("\nEmployee Two \n\nName: {0} {1} \nMonthly Salary: {2} \nYearly Salary: {3}", employee2.FirstName, employee2.LastName, employee2.MonthlySalary, employee2.MonthlySalary * 12);
+    }
 }
